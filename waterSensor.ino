df36e72b1928b05b096ef3fedd3ce2484044d8be
John Tran <johntran@gmail.com>
2020-11-28 21:43:44 -0700

    add water depth value processing, led for value, touch up lcd display

--- waterSensor.ino
+++ waterSensor.ino
@@ -1,6 +1,23 @@
 // include the library code:
 #include <LiquidCrystal.h>
+// include the humidity and temperature sensor library
 #include <Arduino_HTS221.h>
+
+// borrowed significantly from https://www.instructables.com/Christmas-Tree-Water-Level-Indicator/
+// using this sensor: https://www.amazon.com/gp/product/B07THDH7Y4/
+
+// assumes depth sensor is on analog pin 0 ("S" pin of the sensor)
+const int depthSensorPin = A0; 
+const int depthSensorEmpty = 0;
+const int depthSensorFull = 200;
+int depthSensorLastValue = 0;
+
+// setLED inverts the LED request, so 1 == "on"
+void setLED (int r, int g, int b) {
+  digitalWrite(LEDR, 1-r);
+  digitalWrite(LEDG, 1-g);
+  digitalWrite(LEDB, 1-b);
+}
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -15,41 +32,65 @@ void setup() {
   
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, John!");
 }
 
 void loop() {
+
+  // read the depth sensor
+  int depthValue = analogRead(depthSensorPin);
+  depthSensorLastValue = depthValue;
+  int depthValuePercent = map (depthValue, depthSensorEmpty, depthSensorFull, 0, 100);
 
   // Passing in FAHRENHEIT as the unit parameter to ENV.readTemperature(...),
   // allows you to read the sensor values in imperial units
   float temperature = HTS.readTemperature(FAHRENHEIT);
   float humidity    = HTS.readHumidity();
 
-  String firstLine = "Temperature=";
+  // print the temperature to lcd line 0 and serial
+  String firstLine = "Temp=";
   firstLine += int(round(temperature));
   firstLine += char(0xDF);
   firstLine += "F            ";
-  // print each of the sensor values
-  Serial.print("Temperature = ");
+  Serial.print("Temp=");
   Serial.print(temperature);
-  Serial.println("°F");
+  Serial.print("°F");
 
-  String secondLine = "Humidity=";
-  secondLine += int(round(humidity));
-  secondLine += "%             ";
-  Serial.print("Humidity    = ");
+  // print the humidity to lcd line 0 and serial
+  firstLine += " Humi=";
+  firstLine += int(round(humidity));
+  firstLine += "%             ";
+  Serial.print(" Humi=");
   Serial.print(humidity);
   Serial.println("%");
 
-  // print an empty line
-  Serial.println();
-  
+  // print the water depth to lcd line 1 and serial
+  String secondLine = "Depth=";
+  secondLine += depthValue;
+  secondLine += " (";
+  secondLine += depthValuePercent;
+  secondLine += "%)";
+  Serial.print("Depth=");
+  Serial.print(depthValue);
+  Serial.print(" (");
+  Serial.print(depthValuePercent);
+  Serial.print("%)");
+
+  // print the lines to the lcd
   lcd.setCursor(0, 0);
   lcd.print(firstLine);
   
   lcd.setCursor(0, 1);
   lcd.print(secondLine);
 
-  delay(2000);
+  //set LED depending on percentage
+  if (depthValuePercent > 80)  {
+    setLED (0,1,0); // green
+  } else if (depthValuePercent > 10) { 
+    setLED (1,1,0); // yellow
+  } else { // below 10%
+    setLED (1,0,0); // red
+  }
+  
+  // this sets the frequency at which we sample
+  delay(500);
 }
